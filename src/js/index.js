395619a48e3ab5f3be30b8bb28d4f847c03da494
yanglei0323 <1085943717@qq.com>
2017-09-26 13:37:43 +0800

    使用IFRAME中的alert替换原有页面的alert

--- src/js/index.js
+++ src/js/index.js
@@ -402,6 +402,15 @@ index.config(['$routeProvider', function ($routeProvider) {
 			}
 		});
 	}
+	//alert去文本链接
+	window.alert = function(name){
+		var iframe = document.createElement("IFRAME");
+		iframe.style.display="none";
+		iframe.setAttribute("src", 'data:text/plain,');
+		document.documentElement.appendChild(iframe);
+		window.frames[0].window.alert(name);
+		iframe.parentNode.removeChild(iframe);
+	};
 	
 	// if (navigator.geolocation) {
  //    	navigator.geolocation.getCurrentPosition(showPosition);
